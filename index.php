2ade109c81d72a4cb22a4d9c688c0f62f1a1bdfc
Michel Gort <michelgort@gmail.com>
2013-05-13 22:05:43 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: Michel
+ * Date: 10-5-13
+ * Time: 22:20
+ * To change this template use File | Settings | File Templates.
+ */
+
+echo "Success!";
